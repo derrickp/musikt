d87e441db818e5d1777b5704bfa801be1c55dbaa
Derrick Plotsky <derrickplotsky@gmail.com>
2020-01-21 23:02:11 -0800

    Update some entities
    After doing a couple queries to the actual endpoint, I noticed that that
    these fields were no longer coming back.

--- src/main/kotlin/dev/plotsky/musikt/entities/Recording.kt
+++ src/main/kotlin/dev/plotsky/musikt/entities/Recording.kt
@@ -4,10 +4,11 @@ import com.squareup.moshi.Json
 
 data class Recording(
     val id: String,
-    val score: Int,
+    val score: Int?,
     val title: String,
     val length: Int,
-    @Json(name = "artist-credit") val artistCredit: List<ArtistCredit>,
-    val releases: List<ReleaseReference>
+    @Json(name = "artist-credit")
+    val artistCredit: List<ArtistCredit> = emptyList(),
+    val releases: List<ReleaseReference> = emptyList()
 
 )
--- src/main/kotlin/dev/plotsky/musikt/entities/ReleaseReference.kt
+++ src/main/kotlin/dev/plotsky/musikt/entities/ReleaseReference.kt
@@ -4,13 +4,14 @@ import com.squareup.moshi.Json
 
 data class ReleaseReference(
     val id: String,
-    val count: Int,
+    val count: Int?,
     val title: String,
     val status: String,
-    @Json(name = "release-group") val releaseGroup: ReleaseGroup,
+    @Json(name = "release-group") val releaseGroup: ReleaseGroup?,
     val date: String,
     val country: String,
-    @Json(name = "release-events") val releaseEvents: List<ReleaseEvent>,
-    @Json(name = "track-count") val trackCount: Int,
-    val media: List<Media>
+    @Json(name = "release-events")
+    val releaseEvents: List<ReleaseEvent> = emptyList(),
+    @Json(name = "track-count") val trackCount: Int?,
+    val media: List<Media> = emptyList()
 )
