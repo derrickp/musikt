5dbc6f5a7867f1514195287861025b4f4c56b553
Derrick Plotsky <derrickplotsky@gmail.com>
2020-02-11 19:04:25 -0800

    Types can be null apparently

--- src/main/kotlin/dev/plotsky/musikt/entities/areas/RelatedArea.kt
+++ src/main/kotlin/dev/plotsky/musikt/entities/areas/RelatedArea.kt
@@ -5,7 +5,7 @@ import com.squareup.moshi.Json
 data class RelatedArea(
     val area: AreaReference,
     val direction: String,
-    val type: String = "",
-    @Json(name = "type-id") val typeId: String = "",
-    @Json(name = "target-type") val targetType: String = ""
+    val type: String? = "",
+    @Json(name = "type-id") val typeId: String? = "",
+    @Json(name = "target-type") val targetType: String? = ""
 )
